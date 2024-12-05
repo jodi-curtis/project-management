a738a9fb1fb58d380833bf6ad479e16ef22e01e0
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-12-05 21:37:01 +0000

    Added task completed date

--- tasks/models.py
+++ tasks/models.py
@@ -7,9 +7,18 @@ from datetime import timedelta, date
 class Task(models.Model):
     name = models.CharField(max_length=200)
     due_date = models.DateField(default= date.today() + timedelta(1))
+    completed_date = models.DateField(null=True, blank=True) 
     assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     complete = models.BooleanField(default=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.complete and not self.completed_date:
+            self.completed_date = date.today()
+        elif not self.complete:
+            self.completed_date = None
+            
+        super().save(*args, **kwargs)
