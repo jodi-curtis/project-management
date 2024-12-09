7415bf41c7080c4620897db4dd27639abe175093
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-12-09 13:39:06 +0000

    Set up for render pt2

--- project_management/settings.py
+++ project_management/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+0&yc4eze+b-85^#^#aex1ya)wfn7(1s@^1yubnlnpw7go4b7k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'TRUE'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('RENDER_HOSTNAME', '')]
 
 
 # Application definition
